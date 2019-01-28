7a4d49f366dbc57ce50c40da5c1ec39d3c27c4af
Hitesh Lalwani <htshllwn@gmail.com>
2019-01-28 21:46:50 +0530

    Completed maxchar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,64 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (const char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
+
+// My Solution
+// function maxChar(str) {
+//     let tempMap = {};
+//     str.split('').forEach(c => 
+//         tempMap[c] ? tempMap[c]++ : tempMap[c] = 1);
+//     let maxChar = '';
+//     let maxCount = 0;
+//     for (const k in tempMap) {
+//         if (tempMap.hasOwnProperty(k)) {
+//             const e = tempMap[k];
+//             if (e > maxCount) {
+//                 maxCount = e;
+//                 maxChar = k;
+//             }
+//         }
+//     }
+//     return maxChar;
+// }
+
+// Solution 1
+// function maxChar(str) {
+//     const charMap = {};
+
+//     let max = 0;
+//     let maxChar = '';
+
+//     for (let char of str) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     for (const char in charMap) {
+//         if (charMap[char] > max) {
+//             max = charMap[char];
+//             maxChar = char;
+//         }
+//     }
+
+//     return maxChar;
+// }
