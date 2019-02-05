8eb300f48ffa8f56e237bc289c2782cc316a1144
Hitesh Lalwani <htshllwn@gmail.com>
2019-02-05 22:43:10 +0530

    Completed steps

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,72 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+}
 
 module.exports = steps;
+
+// My Solution
+// function steps(n) {
+//     for (let i = 0; i < n; i++) {
+//         let res = '';
+//         for (let j = 0; j <= i; j++) {
+//             res += '#';
+//         }
+//         for (let j = (n - 1) - i; j > 0; j--) {
+//             res += ' ';
+//         }
+//         console.log(res);
+//     }
+// }
+
+// Solution 1
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += "#";
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+// Solution 2
+// function steps(n, row = 0, stair = '') {
+//     if (n === row) {
+//         return;
+//     }
+
+//     if (n === stair.length) {
+//         console.log(stair);
+//         return steps(n, row + 1);
+//     }
+
+//     if (stair.length <= row) {
+//         stair += '#';
+//     } else {
+//         stair += ' ';
+//     }
+
+//     steps(n, row, stair);
+// }
