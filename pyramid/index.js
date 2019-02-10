beaae7c0a27e797a1e620829f092548d2cfe2c76
Hitesh Lalwani <htshllwn@gmail.com>
2019-02-10 21:13:32 +0530

    Completed pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,81 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// My Solution
+// function pyramid(n) {
+//     for (let i = 0; i < n; i++) {
+//         let row = '';
+//         let noOfStars = (i * 2) + 1;
+//         let noOfSpaces = (((n - 1) * 2) + 1 - noOfStars) / 2;
+//         for (let j = noOfSpaces; j >= 1; j--) {
+//             row += ' ';
+//         }
+//         for (let j = noOfStars; j > 0; j--) {
+//             row += '#';
+//         }
+//         for (let j = noOfSpaces; j >= 1; j--) {
+//             row += ' ';
+//         }
+//         console.log(row);
+//     }
+// }
+
+// Solution 1
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+        
+//         for (let column = 0; column < 2 * n - 1; column++) {
+//             if (midpoint - row <= column && midpoint + row >= column) {
+//                 level += '#';
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+
+// Solution 2
+// function pyramid(n, row = 0, level = '') {
+//     if (row === n) {
+//         return;
+//     }
+
+//     if (level.length === 2 * n - 1) {
+//         console.log(level);
+//         return pyramid(n, row + 1);
+//     }
+
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//     let add;
+//     if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//         add = '#';
+//     } else {
+//         add = ' ';
+//     }
+//     pyramid(n, row, level + add);
+// }
